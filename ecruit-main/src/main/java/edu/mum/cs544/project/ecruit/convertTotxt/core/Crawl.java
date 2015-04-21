0739090a322cfdcc0f19ff28b5c3921421726024
kaushaln <kaushal.niraula@gmail.com>
2015-04-21 14:25:19 -0500

    Domain done

--- ecruit-main/src/main/java/edu/mum/cs544/project/ecruit/convertTotxt/core/Crawl.java
+++ ecruit-main/src/main/java/edu/mum/cs544/project/ecruit/convertTotxt/core/Crawl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.hibernate.SessionFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,32 +11,36 @@ import org.jsoup.select.Elements;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import edu.mum.cs544.project.ecruiter.service.ProfileService;
+
 public class Crawl {
 
-	static PersistorService service;
+	static ProfileService service;
 	
 	static Set<String>  s = new HashSet<String>();
 
 	public static void main(String[] args) throws IOException {
 		ApplicationContext context= new ClassPathXmlApplicationContext("context.xml");
-		service= context.getBean("service",PersistorService.class);
+		service= context.getBean("service",ProfileService.class);
 		crawl("http://np.linkedin.com/in/aprithvi");
+	}
+
+	public static ProfileService getService() {
+		return service;
+	}
+
+	public static void setService(ProfileService service) {
+		Crawl.service = service;
 	}
 
 	public static void crawl(String url) throws IOException {
 		System.out.println(url);
 		Document doc = Jsoup.connect(url).timeout(10000).get();
-		DocParser parse = new DocParser();
-		Profile p;
 		try {
-			p = parse.parser(doc);
-			service.save(p);
+			DocParser.setDocument(doc);
+			service.save(url,DocParser.getName(),DocParser.getIndustry(),DocParser.getSkillSet(),DocParser.getEducation(),DocParser.getExperience());
 		} catch (Exception e) {
 			e.printStackTrace();
-			// nothing to do....
-			// just add the url to error database for developers input if the
-			// url was supposed to be parsed.
-			// continue parsing.
 		}
 
 		Elements questions = doc.select("a[href]");
--- ecruit-main/src/main/java/edu/mum/cs544/project/ecruit/convertTotxt/core/DocParser.java
+++ ecruit-main/src/main/java/edu/mum/cs544/project/ecruit/convertTotxt/core/DocParser.java
@@ -1,34 +1,72 @@
 package edu.mum.cs544.project.ecruit.convertTotxt.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class DocParser {
-	public Profile parser(Document doc) throws Exception {
-		Profile profile = new Profile();
-		for (Element e : doc.getElementsByClass("full-name")) {
-			profile.setFullName(e.text());
-		}
-		Element eLocation = doc.getElementById("location");
-		for (Element e : eLocation.getElementsByClass("locality")) {
-			profile.setLocation(e.text());
-		}
-		for (Element e : eLocation.getElementsByClass("industry")) {
-			profile.setIndrustry(e.text());
-		}
-		for (Element e : doc.getElementsByClass("endorse-item")) {
-			profile.addSkillSet(e.text());
-		}
-		Element eEducation = doc.getElementById("background-education");
-		for (Element e : eEducation.getElementsByClass("degree")) {
-			profile.addEdulvls(e.text());
-		}
+	private static Document document;
 
-		Element experience = doc.getElementById("background-experience");
+	public static void setDocument(Document doc){
+		document=doc;
+	}
+
+	public static String getName() {
+		String name = "";
+		for (Element e : document.getElementsByClass("full-name")) {
+			name = e.text();
+		}
+		return name;
+	}
+
+	public static String getLocation() {
+		String location = "";
+
+		for (Element e : (document.getElementById("location"))
+				.getElementsByClass("locality")) {
+			location = e.text();
+
+		}
+		return location;
+	}
+
+	public static String getIndustry() {
+		String industry = "";
+
+		for (Element e : document.getElementsByClass("industry")) {
+			industry = e.text();
+
+		}
+		return industry;
+	}
+
+	public static List<String> getSkillSet() {
+		List<String> skills = new ArrayList<String>();
+		for (Element e : document.getElementsByClass("endorse-item")) {
+			skills.add(e.text());
+		}
+		return skills;
+	}
+
+	public static List<String> getEducation() {
+		List<String> edu = new ArrayList<String>();
+		for (Element e : (document.getElementById("background-education")
+				.getElementsByClass("degree"))) {
+			edu.add(e.text());
+		}
+		return edu;
+
+	}
+
+	public static int getExperience() {
+
+		Element experience = document.getElementById("background-experience");
 		int noOfMonth = 0;
 		for (Element e : experience
 				.getElementsByClass("experience-date-locale")) {
-			
+
 			String elementText = e.text();
 			int bracesPositionOpen = elementText.indexOf("(");
 			int bracesPositionClose = elementText.indexOf(")");
@@ -36,16 +74,17 @@ public class DocParser {
 					.substring(bracesPositionOpen + 1, bracesPositionClose)
 					.trim().toUpperCase();
 			if (elementText.contains("YEAR")) {
-				String yr=elementText.replaceAll("YEAR.*", "");
-				noOfMonth+=Integer.parseInt(yr.trim())*12;
+				String yr = elementText.replaceAll("YEAR.*", "");
+				noOfMonth += Integer.parseInt(yr.trim()) * 12;
 			}
 			if (elementText.contains("MONTH")) {
-				String month=elementText.replaceAll(".*YEAR.", "").trim();
-				month=month.replaceAll("MONTH.*", "");
-				noOfMonth+=Integer.parseInt(month.trim());
+				String month = elementText.replaceAll(".*YEAR.", "").trim();
+				month = month.replaceAll("MONTH.*", "");
+				noOfMonth += Integer.parseInt(month.trim());
 			}
 		}
-		profile.setExperience(noOfMonth);
-		return profile;
+		return (noOfMonth);
 	}
+
+	
 }
